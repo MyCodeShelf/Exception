df874d5328ad3d738c9faf19a38c715172af3a77
rajputszapp <vikaschauhanmail@gmail.com>
2020-06-12 10:19:44 +0530

    Throws related concepts

--- src/packExceptionData/Data.java
+++ src/packExceptionData/Data.java
@@ -1,5 +1,7 @@
 package packExceptionData;
 
+import java.io.IOException;
+
 public class Data {
 int d;
 public Data(int num) {
@@ -23,5 +25,22 @@ public int getD() //throws NegativeDataException (do not really handle the excep
 	}
 	return d;
 }
-
+//why  use throws demonstration 
+//IF we have class with multiple methods which may throw an exception then creating try and catch block
+//for every individual method will be time consuming ,unnecessary long and will be less-readable.
+//so we use throws which delegate the work of exception handling to caller.while calling the method ,the caller will have to handle it
+public void method1(int m)
+{
+	if(m==1)
+        throw new ArithmeticException("Arithmetic Exception Occurred");
+     else
+        throw new 	NullPointerException("FoundIOException");
+}
+public void method2(int m)
+{
+	if(m==1)
+        throw new ArithmeticException("Arithmetic Exception Occurred");
+     else
+        throw new 	NullPointerException("FoundIOException");
+}
 }
--- src/packExceptionData/Drive.java
+++ src/packExceptionData/Drive.java
@@ -1,11 +1,33 @@
 package packExceptionData;
 
+import java.io.IOException;
+
 public class Drive {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 Data dd=new Data(-8);
 System.out.println(dd.getD());
-	}
-
+//throws delegates the handling of exception to main
+try
+{
+	dd.method1(1);
+	
+}
+catch (ArithmeticException e) {
+	e.printStackTrace();
+}
+catch (NullPointerException ee) {
+	ee.printStackTrace();
+}
+try
+{
+dd.method2(2);
+}
+catch (NullPointerException e) {
+	// TODO: handle exception
+	System.out.println("No null allowed");
+}
+}
+	
 }
